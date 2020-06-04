25018c34658bbc05035a7a5637df96fcba300448
Roman Yurchak <rth.yurchak@gmail.com>
2020-06-04 17:51:35 +0200

    Add logging example

---
+++ examples/logging-pipeline.py
@@ -0,0 +1,23 @@
+from sklearn.compose import make_column_transformer
+from sklearn.datasets import make_classification
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import SGDClassifier
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import MinMaxScaler, StandardScaler
+from sklearn_callbacks import DebugCallback
+
+X, y = make_classification(n_samples=10000, n_features=100, random_state=0)
+
+pipe = make_pipeline(
+    SimpleImputer(),
+    make_column_transformer(
+        (StandardScaler(), slice(0, 80)), (MinMaxScaler(), slice(80, 90)),
+    ),
+    SGDClassifier(max_iter=20),
+)
+
+
+pbar = DebugCallback()
+pipe._set_callbacks(pbar)
+
+_ = pipe.fit(X, y)
